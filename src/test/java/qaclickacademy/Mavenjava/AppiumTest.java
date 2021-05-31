17729a6656ee8b0c4222b50fa3cc9c0ece911c6e
Ajay Upadrasta <ajay1stylish@gmail.com>
2021-05-31 14:06:37 +0530

    Commit message

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidNative() {
+		System.out.println("AndroidNative");
+		
+	}
+	
+	@Test
+	public void IOSNative() {
+		System.out.println("IOSNative");
+
+	}
+
+}
